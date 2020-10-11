79ff6edc6eaec8fc1ef03d8f7ce4c868d38e801f
mukhamad rizal <mukhamad.ilham@students.amikom.ac.id>
2020-10-11 16:26:18 +0700

    Add project files.

---
+++ TheCashier/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCashier
+{
+    class Calculator
+    {
+        private List<Item> listItem;
+        private double total = 0;
+
+        public Calculator()
+        {
+            this.listItem = new List<Item>();
+        }
+        public void addItem(Item item)
+        {
+            this.listItem.Add(item);
+            this.total += item.getSubTotal();
+        }
+        public double getTotal()
+        {
+            return total;
+        }
+
+        public List<Item> getListItem()
+        {
+            return listItem;
+        }
+    }
+}
